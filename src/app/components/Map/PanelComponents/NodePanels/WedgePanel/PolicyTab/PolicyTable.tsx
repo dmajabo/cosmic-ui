db4a94c8875766aca56d3e65bbe06b32693be5f7
yurii.rabchevskiy <yrabchevskiy@gmail.com>
2021-09-21 16:10:01 +0300

    add base tables for wedge

---
+++ src/app/components/Map/PanelComponents/NodePanels/WedgePanel/PolicyTab/PolicyTable.tsx
@@ -0,0 +1,87 @@
+import React from 'react';
+import { AbsLoaderWrapper } from 'app/components/Loading/styles';
+import LoadingIndicator from 'app/components/Loading';
+import { TableHeaderStyles, TableWrapperStyles } from 'app/components/Basic/Table/styles';
+import { IVmRule } from 'lib/api/ApiModels/Metrics/apiModel';
+import { TableContainer, Table, TableHead, TableRow, TableCell, TableBody } from '@material-ui/core';
+import { ErrorMessage } from 'app/components/Basic/ErrorMessage/ErrorMessage';
+import { EmptyText } from 'app/components/Basic/NoDataStyles/NoDataStyles';
+import { TableStyles } from 'app/components/Basic/Table/TableStyles';
+
+interface Props {
+  data: IVmRule[];
+  showLoader: boolean;
+  error?: string;
+  styles?: Object;
+  title: string;
+}
+
+const PolicyTable: React.FC<Props> = (props: Props) => {
+  const classes = TableStyles();
+  return (
+    <TableWrapperStyles style={props.styles}>
+      <TableHeaderStyles>{props.title}</TableHeaderStyles>
+      <TableContainer className={classes.container}>
+        <Table stickyHeader aria-label="sticky table" className={classes.table}>
+          <TableHead>
+            <TableRow>
+              <TableCell className={classes.tableHeadCell} style={{ minWidth: '20px' }}>
+                #
+              </TableCell>
+              <TableCell style={{ minWidth: '70px' }} className={classes.tableHeadCell}>
+                Policy
+              </TableCell>
+              <TableCell className={classes.tableHeadCell} style={{ minWidth: '110px' }}>
+                Protocol
+              </TableCell>
+              <TableCell style={{ minWidth: '110px' }} className={classes.tableHeadCell}>
+                Destination
+              </TableCell>
+              <TableCell className={classes.tableHeadCell} style={{ minWidth: '110px' }}>
+                Port Range
+              </TableCell>
+            </TableRow>
+          </TableHead>
+          <TableBody>
+            {props.data && props.data.length
+              ? props.data.map((row, rowIndex) => {
+                  return (
+                    <TableRow hover tabIndex={-1} key={`tableRow${row.id}`} className={classes.row}>
+                      <TableCell className={classes.tableCell}>{rowIndex + 1}</TableCell>
+                      <TableCell className={classes.tableCell}>Allow</TableCell>
+                      <TableCell className={classes.tableCell} style={{ textTransform: 'uppercase' }}>
+                        {row.ipProtocol}
+                      </TableCell>
+                      <TableCell className={classes.tableCell}>{row.cidrs && row.cidrs.length ? row.cidrs[0].name : null}</TableCell>
+                      <TableCell className={classes.tableCell}>{row.fromPort === 0 && row.toPort === 0 ? 'All' : `${row.fromPort} - ${row.toPort}`}</TableCell>
+                    </TableRow>
+                  );
+                })
+              : null}
+            {(!props.data || !props.data.length) && !props.showLoader && !props.error && (
+              <TableRow className={classes.row}>
+                <TableCell className={classes.tableCell} colSpan={5}>
+                  <EmptyText>No data</EmptyText>
+                </TableCell>
+              </TableRow>
+            )}
+            {(!props.data || !props.data.length) && !props.showLoader && props.error && (
+              <TableRow className={classes.row}>
+                <TableCell className={classes.tableCell} colSpan={5}>
+                  <ErrorMessage>{props.error}</ErrorMessage>
+                </TableCell>
+              </TableRow>
+            )}
+          </TableBody>
+        </Table>
+      </TableContainer>
+      {props.showLoader && (
+        <AbsLoaderWrapper width="100%" height="calc(100% - 42px)" top="42px">
+          <LoadingIndicator margin="auto" width="24px" height="24px" />
+        </AbsLoaderWrapper>
+      )}
+    </TableWrapperStyles>
+  );
+};
+
+export default React.memo(PolicyTable);
---
+++ src/app/components/Map/PanelComponents/NodePanels/WedgePanel/RoutesTab/RouteTableWrapper.tsx
@@ -0,0 +1,72 @@
+import React from 'react';
+import { AbsLoaderWrapper } from 'app/components/Loading/styles';
+import LoadingIndicator from 'app/components/Loading';
+import { TableWrapperStyles } from 'app/components/Basic/Table/styles';
+import { IRouteResDataItem } from 'lib/api/ApiModels/Metrics/apiModel';
+import { TableContainer, Table, TableHead, TableRow, TableCell, TableBody } from '@material-ui/core';
+import { ErrorMessage } from 'app/components/Basic/ErrorMessage/ErrorMessage';
+import { EmptyText } from 'app/components/Basic/NoDataStyles/NoDataStyles';
+import { TableStyles } from 'app/components/Basic/Table/TableStyles';
+import RouteTable from './RouteTable';
+
+interface Props {
+  data: IRouteResDataItem[];
+  showLoader: boolean;
+  error?: string;
+  styles?: Object;
+}
+
+const RouteTableWrapper: React.FC<Props> = (props: Props) => {
+  const classes = TableStyles();
+  if (!props.data || props.data.length === 0 || props.error || props.showLoader) {
+    return (
+      <TableWrapperStyles style={props.styles}>
+        <TableContainer className={classes.container}>
+          <Table stickyHeader aria-label="sticky table" className={classes.table}>
+            <TableHead>
+              <TableRow>
+                <TableCell className={classes.tableHeadCell} style={{ minWidth: 110 }}>
+                  Destination
+                </TableCell>
+                <TableCell className={classes.tableHeadCell} style={{ minWidth: 100 }}>
+                  Next Hop
+                </TableCell>
+                <TableCell className={classes.tableHeadCell}>Status</TableCell>
+              </TableRow>
+            </TableHead>
+            <TableBody>
+              {(!props.data || !props.data.length) && !props.showLoader && !props.error && (
+                <TableRow className={classes.row}>
+                  <TableCell className={classes.tableCell} colSpan={5}>
+                    <EmptyText>No data</EmptyText>
+                  </TableCell>
+                </TableRow>
+              )}
+              {(!props.data || !props.data.length) && !props.showLoader && props.error && (
+                <TableRow className={classes.row}>
+                  <TableCell className={classes.tableCell} colSpan={5}>
+                    <ErrorMessage>{props.error}</ErrorMessage>
+                  </TableCell>
+                </TableRow>
+              )}
+            </TableBody>
+          </Table>
+        </TableContainer>
+        {props.showLoader && (
+          <AbsLoaderWrapper width="100%" height="calc(100% - 42px)" top="42px">
+            <LoadingIndicator margin="auto" width="24px" height="24px" />
+          </AbsLoaderWrapper>
+        )}
+      </TableWrapperStyles>
+    );
+  }
+  return (
+    <>
+      {props.data.map(table => (
+        <RouteTable key={`table${table.id}`} data={table} styles={props.styles} />
+      ))}
+    </>
+  );
+};
+
+export default React.memo(RouteTableWrapper);
