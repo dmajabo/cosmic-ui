a0985dfd41ae9aa1ee99de90e9c2cdadc722dd0d
yurii.rabchevskiy <yrabchevskiy@gmail.com>
2021-09-27 21:12:01 +0300

    update page detection behavior. add new route

--- src/app/components/Sidebar/index.tsx
+++ src/app/components/Sidebar/index.tsx
@@ -1,43 +1,28 @@
 import * as React from 'react';
-import { automationIcon } from 'app/components/SVGIcons/pagesIcons/automation';
-import { dashboardIcon } from 'app/components/SVGIcons/pagesIcons/dashboard';
 import { logoIcon } from 'app/components/SVGIcons/pagesIcons/logo';
-import { policyIcon } from 'app/components/SVGIcons/pagesIcons/policy';
-import { topologyIcon } from 'app/components/SVGIcons/pagesIcons/topology';
-import { performanceDashboard } from 'app/components/SVGIcons/pagesIcons/performanceDashboard';
-import { settingsIcon } from 'app/components/SVGIcons/pagesIcons/settings';
-import { analyticsIcon } from 'app/components/SVGIcons/pagesIcons/analytics';
 import { demoIcon } from 'app/components/SVGIcons/pagesIcons/demo';
-import { ROUTE } from 'lib/Routes/model';
-import { useRouteMatch } from 'react-router-dom';
-import history from 'utils/history';
+import { APP_PAGES, IPage } from 'lib/Routes/model';
 import ListLink from './ListLink';
 import { Demo, List, Logo, WrapSidebar } from './styles';
 
 interface SidebarProps {
+  activePageId: string;
   isOpenSidebar: boolean;
-  onChangeStateSidebar: () => void;
-  onCloseSidebar: () => void;
+  onToogleSideBar: () => void;
+  onGoTo: (page: IPage) => void;
 }
 const Sidebar: React.FC<SidebarProps> = props => {
-  const match: any = useRouteMatch();
-
-  const onChangeSidebarPath = (path: string) => {
-    props.onCloseSidebar();
-    history.push(match.url + path);
+  const onGoTo = (path: IPage) => {
+    props.onGoTo(path);
   };
 
   return (
     <WrapSidebar isOpen={props.isOpenSidebar}>
-      <Logo onClick={props.onChangeStateSidebar}>{logoIcon()}</Logo>
+      <Logo onClick={props.onToogleSideBar}>{logoIcon()}</Logo>
       <List>
-        <ListLink icon={dashboardIcon} label="Dashboard" path={`${match.url}${ROUTE.dashboard}`} onClick={() => onChangeSidebarPath(ROUTE.dashboard)} />
-        <ListLink icon={topologyIcon} label="Topology" path={`${match.url}${ROUTE.topology}`} onClick={() => onChangeSidebarPath(ROUTE.topology)} />
-        <ListLink icon={performanceDashboard} label="Performance Dashboard" path={`${match.url}${ROUTE.performanceDashboard}`} onClick={() => onChangeSidebarPath(ROUTE.performanceDashboard)} />
-        <ListLink icon={policyIcon} label="Policy" path={`${match.url}${ROUTE.policy}`} onClick={() => onChangeSidebarPath(ROUTE.policy)} />
-        <ListLink icon={analyticsIcon} label="Analytics" path={`${match.url}${ROUTE.analytics}`} onClick={() => onChangeSidebarPath(ROUTE.analytics)} />
-        <ListLink icon={automationIcon} label="Automation" path={`${match.url}${ROUTE.automation}`} onClick={() => onChangeSidebarPath(ROUTE.automation)} />
-        <ListLink icon={settingsIcon} label="Settings" path={`${match.url}${ROUTE.settings}`} onClick={() => onChangeSidebarPath(ROUTE.settings)} />
+        {APP_PAGES.map(page => (
+          <ListLink key={`app_page${page.id}`} isActive={props.activePageId === page.id} icon={page.icon} label={page.pageName} onClick={() => onGoTo(page)} />
+        ))}
       </List>
       <Demo>{demoIcon}</Demo>
     </WrapSidebar>
