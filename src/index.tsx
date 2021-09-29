d84d6d066c88242f22cccf38c7c6141d36c8c13c
Ajay-TechP <ajay.vaidya@techprescient.com>
2021-09-29 10:37:54 +0530

    changed auth0 redirect location

--- src/index.tsx
+++ src/index.tsx
@@ -34,7 +34,7 @@ const MOUNT_NODE = document.getElementById('root') as HTMLElement;
 
 ReactDOM.render(
   <Provider store={store}>
-    <Auth0Provider domain={process.env.REACT_APP_AUTH0_DOMAIN || ''} clientId={process.env.REACT_APP_AUTH0_CLIENT_ID || ''} redirectUri={window.location.origin}>
+    <Auth0Provider domain={process.env.REACT_APP_AUTH0_DOMAIN || ''} clientId={process.env.REACT_APP_AUTH0_CLIENT_ID || ''} redirectUri={window.location.origin + '/app'}>
       <HelmetProvider>
         <React.StrictMode>
           <App />
