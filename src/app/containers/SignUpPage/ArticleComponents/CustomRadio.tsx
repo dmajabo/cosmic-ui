f2723d9fa8f0d439ed29db52465f605c8ab6d63b
Ajay-TechP <ajay.vaidya@techprescient.com>
2021-10-14 21:46:13 +0530

    added Connect AWS form UI

---
+++ src/app/containers/SignUpPage/ArticleComponents/CustomRadio.tsx
@@ -0,0 +1,72 @@
+import * as React from 'react';
+import { styled } from '@mui/material/styles';
+import Radio, { RadioProps } from '@mui/material/Radio';
+import RadioGroup from '@mui/material/RadioGroup';
+import FormControlLabel from '@mui/material/FormControlLabel';
+import FormControl from '@mui/material/FormControl';
+import { SignUpStyles } from '../SignUpStyles';
+import { Options } from '..';
+
+const BpIcon = styled('span')(({ theme }) => ({
+  borderRadius: '50%',
+  width: 16,
+  height: 16,
+  '.Mui-focusVisible &': {
+    outline: '2px auto rgba(19,124,189,.6)',
+    outlineOffset: 2,
+  },
+  'input:hover ~ &': {
+    backgroundColor: theme.palette.mode === 'dark' ? '#30404d' : '#ebf1f5',
+  },
+  boxShadow: 'none',
+  background: theme.palette.mode === 'dark' ? 'rgba(57,75,89,.5)' : 'rgba(206,217,224,.5)',
+}));
+
+const BpCheckedIcon = styled(BpIcon)({
+  backgroundColor: '#437FEC',
+  backgroundImage: 'linear-gradient(180deg,hsla(0,0%,100%,.1),hsla(0,0%,100%,0))',
+  '&:before': {
+    display: 'block',
+    width: 16,
+    height: 16,
+    backgroundImage: 'radial-gradient(#fff,#fff 45%,transparent 15%)',
+    content: '""',
+  },
+  'input:hover ~ &': {
+    backgroundColor: '#106ba3',
+  },
+});
+
+function BpRadio(props: RadioProps) {
+  return (
+    <Radio
+      sx={{
+        '&:hover': {
+          bgcolor: 'transparent',
+        },
+      }}
+      disableRipple
+      color="primary"
+      checkedIcon={<BpCheckedIcon />}
+      icon={<BpIcon />}
+      {...props}
+    />
+  );
+}
+
+interface CustomRadioProps {
+  readonly radioOptions: Options[];
+}
+
+export const CustomRadio: React.FC<CustomRadioProps> = ({ radioOptions }) => {
+  const classes = SignUpStyles();
+  return (
+    <FormControl component="fieldset">
+      <RadioGroup defaultValue={radioOptions[0].value} aria-label="options" name="customized-radios">
+        {radioOptions.map(option => (
+          <FormControlLabel key={option.value} value={option.value} control={<BpRadio />} label={option.label} />
+        ))}
+      </RadioGroup>
+    </FormControl>
+  );
+};
--- src/app/containers/SignUpPage/index.tsx
+++ src/app/containers/SignUpPage/index.tsx
@@ -6,28 +6,230 @@ import { ConnectEdges } from './ArticleComponents/ConnectEdges';
 import { EdgeBoxProps } from 'types';
 import AwsIcon from './icons/aws.svg';
 import MerakiIcon from './icons/meraki.svg';
+import CopyIcon from './icons/copy.svg';
 import { SignUpStyles } from './SignUpStyles';
 import { CircularProgressWithLabel } from './ArticleComponents/CircularProgressWithLabel';
+import { ConnectSourceForm } from './ArticleComponents/ConnectSourceForm';
+import ReactSelect, { components } from 'react-select';
+import { CustomRadio } from './ArticleComponents/CustomRadio';
+
+const Option = props => {
+  const classes = SignUpStyles();
+  return (
+    <div>
+      <components.Option {...props}>
+        <input className={classes.checkbox} type="checkbox" checked={props.isSelected} onChange={() => null} /> <label>{props.label}</label>
+      </components.Option>
+    </div>
+  );
+};
+
+export interface StepData {
+  readonly stepTitle: string;
+  readonly stepContent: JSX.Element;
+}
+
+export interface Options {
+  readonly value: string;
+  readonly label: string;
+}
+
+const AWS = 'aws';
+const MERAKI = 'meraki';
 
 const SignUpPage: React.FC = () => {
   const [progress, setProgress] = useState<number>(0);
   //TODO: use setProgress on connecting AWS and Meraki
+  const [connectLocation, setConnectLocation] = useState<string>('');
   const classes = SignUpStyles();
+
+  const dropdownStyle = {
+    option: provided => ({
+      ...provided,
+      padding: 20,
+      color: 'black',
+    }),
+    control: provided => ({
+      ...provided,
+      height: 60,
+      width: 500,
+      marginTop: 5,
+    }),
+  };
+
+  const regionOptions: Options[] = [
+    {
+      label: 'USA WEST (Oregon)',
+      value: 'USA WEST (Oregon)',
+    },
+    {
+      label: 'US West (N. California)',
+      value: 'US West (N. California)',
+    },
+    {
+      label: 'US East (Ohio)',
+      value: 'US East (Ohio)',
+    },
+    {
+      label: 'US East (N. Virginia)',
+      value: 'US East (N. Virginia)',
+    },
+
+    {
+      label: 'Asia Pacific (Mumbai)',
+      value: 'Asia Pacific (Mumbai)',
+    },
+  ];
+
+  const flowLogRadioOptions: Options[] = [
+    {
+      value: 'For all VPCs in the selected region(s)',
+      label: 'For all VPCs in the selected region(s)',
+    },
+    {
+      value: 'For selected VPCs in the selected region(s)',
+      label: 'For selected VPCs in the selected region(s)',
+    },
+  ];
+
+  const bucketLogRadioOptions: Options[] = [
+    {
+      value: 'Every minute (recommended)',
+      label: 'Every minute (recommended)',
+    },
+    {
+      value: 'Every 10 minutes (AWS default)',
+      label: 'Every 10 minutes (AWS default)',
+    },
+  ];
+
+  const terraformCommands: string[] = ['terraform init', 'terraform validate', 'terraform plan', 'terraform apply'];
+
+  const awsSteps: StepData[] = [
+    {
+      stepTitle: 'Step 1: Select your region(s)',
+      stepContent: (
+        <>
+          <div className={classes.dropdownLabel}>REGION</div>
+          <ReactSelect
+            isMulti
+            components={{
+              Option,
+            }}
+            menuPortalTarget={document.body}
+            styles={dropdownStyle}
+            options={regionOptions}
+            allowSelectAll={true}
+          />
+        </>
+      ),
+    },
+    {
+      stepTitle: 'Step 2: Select options',
+      stepContent: (
+        <>
+          <div className={classes.radioTitle}>Enable flow logs</div>
+          <CustomRadio radioOptions={flowLogRadioOptions} />
+          <div className={classes.radioTitle}>Write logs to bucket</div>
+          <CustomRadio radioOptions={bucketLogRadioOptions} />
+        </>
+      ),
+    },
+    {
+      stepTitle: 'Step 3: Generate Terraform Configuration',
+      stepContent: (
+        <div>
+          <div className={classes.stepSubtitleContainer}>
+            <span className={classes.stepSubtitleText}>Copy the generated Terraform configuration and save to a file named</span>
+            <code className={classes.codeText}>main.tf</code>
+            <span className={classes.stepSubtitleText}>in an empty directory</span>
+          </div>
+          <div className={classes.codeBox}>
+            <div className={classes.copyIcon}>
+              <img src={CopyIcon} alt="copy" />
+            </div>
+            <pre className={classes.terraformCodeText}>
+              {`
+              terraform { 
+                required_version = ">= 0.12.0" 
+              
+              }
+
+              provider "aws" { 
+                region = "`}
+              <span className={classes.terraformHighlightedCodeText} />
+              {`" 
+              }
+
+              `}
+              <span className={classes.terraformHighlightedCodeText} />
+              {`
+
+              module "kentik_aws_integration" {
+                source = "github.com/kentik/config-snippets-cloud/cloud_AWS/terraform/module"
+                region = "`}
+              <span className={classes.terraformHighlightedCodeText} />
+              {`"
+                rw_s3_access = true
+                multiple_buckets = false
+                vpc_id_list = `}
+              <span className={classes.terraformHighlightedCodeText} />
+              {`
+                store_logs_more_frequently = `}
+              <span className={classes.terraformHighlightedCodeText} />
+              {`
+                create_role = `}
+              <span className={classes.terraformHighlightedCodeText} />
+              {`
+                external_id = 99701
+
+
+              }
+              `}
+            </pre>
+          </div>
+        </div>
+      ),
+    },
+    {
+      stepTitle: 'Step 4: Review and Apply Terraform Configuration',
+      stepContent: (
+        <div>
+          <div className={classes.stepSubtitleContainer}>
+            <span className={classes.stepSubtitleText}>After you've reviewed the configuration in the main.tf file, apply it in the environment where you run Terraform.</span>
+          </div>
+          {terraformCommands.map(command => (
+            <div key={command} className={classes.codeBox}>
+              <div className={classes.copyIcon}>
+                <img src={CopyIcon} alt="copy" />
+              </div>
+              <div className={classes.terraformCodeText}>{command}</div>
+            </div>
+          ))}
+        </div>
+      ),
+    },
+  ];
 
   const edgeBoxArray: EdgeBoxProps[] = [
     {
       img: AwsIcon,
       title: 'AWS',
       content: 'Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.',
-      onClick: () => {},
+      onClick: () => {
+        setConnectLocation(AWS);
+      },
     },
     {
       img: MerakiIcon,
       title: 'Cisco Meraki',
       content: 'Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.',
-      onClick: () => {},
+      onClick: () => {
+        setConnectLocation(MERAKI);
+      },
     },
   ];
+
   return (
     <UnAuthLayout article={<TryDemoComponent />}>
       <div className={classes.topBar}>
@@ -37,9 +239,22 @@ const SignUpPage: React.FC = () => {
           <CircularProgressWithLabel value={progress} />
         </div>
       </div>
-      <SignUpWrapper>
-        <ConnectEdges edgeBoxArray={edgeBoxArray} />
-      </SignUpWrapper>
+      {connectLocation ? (
+        connectLocation === AWS ? (
+          <ConnectSourceForm
+            title="Connect To AWS"
+            img={AwsIcon}
+            subtitle="Configure your AWS integration to enable topology maps and annotate your agent data with important cloud context like regions, availability zones, account, VPC IDs, scaling groups and more."
+            steps={awsSteps}
+          />
+        ) : (
+          <div>Connect with MERAKI</div>
+        )
+      ) : (
+        <SignUpWrapper>
+          <ConnectEdges edgeBoxArray={edgeBoxArray} />
+        </SignUpWrapper>
+      )}
     </UnAuthLayout>
   );
 };
