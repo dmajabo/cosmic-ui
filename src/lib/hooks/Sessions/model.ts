dda2628ae0104fc34d10a11edf500b85f5e32e65
yurii.rabchevskiy <yrabchevskiy@gmail.com>
2021-10-19 20:51:01 +0300

    add support last hour period

--- src/lib/hooks/Sessions/model.ts
+++ src/lib/hooks/Sessions/model.ts
@@ -20,12 +20,14 @@ export const SESSIONS_TABS: ITab<SessionsTabTypes>[] = [
 ];
 
 export enum SessionsSelectValuesTypes {
+  LAST_HOUR = 'SESSION_QUERY_LAST_HOUR',
   LAST_DAY = 'SESSION_QUERY_LAST_DAY',
   LAST_WEEK = 'SESSION_QUERY_LAST_WEEK',
   LAST_MONTH = 'SESSION_QUERY_LAST_MONTH',
 }
 
 export const SESSIONS_SELECT_VALUES: ISelectedListItem<SessionsSelectValuesTypes>[] = [
+  { id: SessionsSelectValuesTypes.LAST_HOUR, value: SessionsSelectValuesTypes.LAST_HOUR, label: 'Last hour' },
   { id: SessionsSelectValuesTypes.LAST_DAY, value: SessionsSelectValuesTypes.LAST_DAY, label: 'Last day' },
   { id: SessionsSelectValuesTypes.LAST_WEEK, value: SessionsSelectValuesTypes.LAST_WEEK, label: 'Last week' },
   { id: SessionsSelectValuesTypes.LAST_MONTH, value: SessionsSelectValuesTypes.LAST_MONTH, label: 'Last month' },
