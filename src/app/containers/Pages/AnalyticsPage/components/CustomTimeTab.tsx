7d35b89cf0b90cdedebfdec8aa368e3ea17ef323
Ajay-TechP <ajay.vaidya@techprescient.com>
2021-11-30 12:29:52 +0530

    solve PR comments

--- src/app/containers/Pages/AnalyticsPage/components/CustomTimeTab.tsx
+++ src/app/containers/Pages/AnalyticsPage/components/CustomTimeTab.tsx
@@ -2,18 +2,18 @@ import { Checkbox } from '@mui/material';
 import React, { useState } from 'react';
 import { AnalyticsStyles } from '../AnalyticsStyles';
 import Select from 'react-select';
-import { SelectOptions } from './MetricsExplorer';
+import { SelectOption } from './MetricsExplorer';
 
 interface CustomTimeTabProps {
-  readonly customFromDate: string;
-  readonly customToDate: string;
-  readonly handleCustomFromDateChange: (value: string) => void;
-  readonly handleCustomToDateChange: (value: string) => void;
-  readonly show: SelectOptions;
-  readonly handleShowChange: (value: SelectOptions) => void;
+  readonly fromDate: string;
+  readonly toDate: string;
+  readonly onFromDateChange: (event: React.ChangeEvent<HTMLInputElement>) => void;
+  readonly onToDateChange: (event: React.ChangeEvent<HTMLInputElement>) => void;
+  readonly timeRange: SelectOption;
+  readonly onTimeRangeChange: (value: SelectOption) => void;
 }
 
-const showOptions: SelectOptions[] = [
+const TIME_RANGE_OPTIONS: SelectOption[] = [
   {
     label: 'Hour',
     value: '-1h',
@@ -32,7 +32,11 @@ const showOptions: SelectOptions[] = [
   },
 ];
 
-export const CustomTimeTab: React.FC<CustomTimeTabProps> = ({ customFromDate, customToDate, handleCustomFromDateChange, handleCustomToDateChange, show, handleShowChange }) => {
+const SHOW_NEXT = 'SHOW THE NEXT';
+const SHOW_PREVIOUS = 'SHOW THE PREVIOUS';
+const DEFAULT_SHOW = 'SHOW';
+
+export const CustomTimeTab: React.FC<CustomTimeTabProps> = ({ fromDate, toDate, onFromDateChange, onToDateChange, timeRange, onTimeRangeChange }) => {
   const classes = AnalyticsStyles();
   const [isFromDateChecked, setIsFromDateChecked] = useState<boolean>(false);
   const [isToDateChecked, setIsToDateChecked] = useState<boolean>(true);
@@ -43,36 +47,33 @@ export const CustomTimeTab: React.FC<CustomTimeTabProps> = ({ customFromDate, cu
 
   const getSelectTitle = () => {
     if (isFromDateChecked && !isToDateChecked) {
-      return 'SHOW THE NEXT';
+      return SHOW_NEXT;
     }
     if (!isFromDateChecked && isToDateChecked) {
-      return 'SHOW THE PREVIOUS';
+      return SHOW_PREVIOUS;
     }
-    return 'SHOW';
+    return DEFAULT_SHOW;
   };
 
   return (
     <div className={classes.customContainer}>
       <div className={classes.blockContainer}>
         <Checkbox checked={isFromDateChecked} onChange={handleFromCheckboxChange} />
-        <span className={classes.dateTimeLabelText}>From:</span>
-        <input
-          value={customFromDate}
-          disabled={!isFromDateChecked}
-          className={classes.dateTimeInput}
-          type="datetime-local"
-          id="customFromDate"
-          onChange={e => handleCustomFromDateChange(e.target.value)}
-        />
+        <div className={classes.labelContainer}>
+          <span className={classes.dateTimeLabelText}>From:</span>
+        </div>
+        <input value={fromDate} disabled={!isFromDateChecked} className={classes.dateTimeInput} type="datetime-local" id="customFromDate" onChange={onFromDateChange} />
       </div>
       <div className={isFromDateChecked && isToDateChecked ? classes.hidden : classes.selectBlockContainer}>
         <div className={classes.tableHeaderText}>{getSelectTitle()}</div>
-        <Select className={classes.showSelect} label="lookup select" value={show} options={showOptions} onChange={e => handleShowChange(e)} />
+        <Select className={classes.showSelect} label="lookup select" value={timeRange} options={TIME_RANGE_OPTIONS} onChange={onTimeRangeChange} />
       </div>
       <div className={classes.blockContainer}>
         <Checkbox checked={isToDateChecked} onChange={handleToCheckboxChange} />
-        <span className={classes.dateTimeLabelText}>To:</span>
-        <input value={customToDate} disabled={!isToDateChecked} className={classes.dateTimeInput} type="datetime-local" id="customToDate" onChange={e => handleCustomToDateChange(e.target.value)} />
+        <div className={classes.labelContainer}>
+          <span className={classes.dateTimeLabelText}>To:</span>
+        </div>
+        <input value={toDate} disabled={!isToDateChecked} className={classes.dateTimeInput} type="datetime-local" id="customToDate" onChange={onToDateChange} />
       </div>
     </div>
   );
--- src/app/containers/Pages/AnalyticsPage/components/MetricsExplorer.tsx
+++ src/app/containers/Pages/AnalyticsPage/components/MetricsExplorer.tsx
@@ -157,17 +157,17 @@ enum ModalName {
 }
 
 enum TimeMetricTabValue {
-  lookback = 'lookback',
-  custom = 'custom',
+  Lookback = 'Lookback',
+  Custom = 'Custom',
 }
 
 interface TabPanelProps {
   readonly children?: React.ReactNode;
-  readonly index: string;
+  readonly name: string;
   readonly value: string;
 }
 
-export interface SelectOptions {
+export interface SelectOption {
   readonly value: string;
   readonly label: string;
 }
@@ -184,10 +184,10 @@ export const getDataSourceCount = (dataSources: DataSourceOptions[]) => {
 };
 
 function TabPanel(props: TabPanelProps) {
-  const { children, value, index, ...other } = props;
+  const { children, value, name, ...other } = props;
 
   return (
-    <div role="tabpanel" hidden={value !== index} id={`simple-tabpanel-${index}`} aria-labelledby={`simple-tab-${index}`} {...other}>
+    <div role="tabpanel" hidden={value !== name} id={`simple-tabpanel-${name}`} aria-labelledby={`simple-tab-${name}`} {...other}>
       {children}
     </div>
   );
@@ -200,6 +200,16 @@ function a11yProps(index: any) {
   };
 }
 
+const INITIAL_LOOKBACK_TIME_RANGE_VALUE = {
+  label: 'Last 5 minutes',
+  value: '-5m',
+};
+
+const INITIAL_SHOW_TIME_RANGE_VALUE = {
+  label: 'Day',
+  value: '-1d',
+};
+
 export const MetricsExplorer: React.FC = () => {
   const classes = AnalyticsStyles();
 
@@ -207,17 +217,11 @@ export const MetricsExplorer: React.FC = () => {
   const [dataSources, setDataSources] = useState<DataSourceOptions[]>([]);
   const [isModalOpen, setIsModalOpen] = useState<boolean>(false);
   const [modalName, setModalName] = useState<ModalName>(ModalName.Dimensions);
-  const [timeTab, setTimeTab] = useState<TimeMetricTabValue>(TimeMetricTabValue.lookback);
-  const [selectedLookback, setSelectedLookback] = useState<SelectOptions>({
-    label: 'Last 5 minutes',
-    value: '-5m',
-  });
+  const [timeTab, setTimeTab] = useState<TimeMetricTabValue>(TimeMetricTabValue.Lookback);
+  const [selectedLookbackTimeRange, setSelectedLookbackTimeRange] = useState<SelectOption>(INITIAL_LOOKBACK_TIME_RANGE_VALUE);
   const [selectedCustomFromDate, setSelectedCustomFromDate] = useState<string>('');
   const [selectedCustomToDate, setSelectedCustomToDate] = useState<string>('');
-  const [selectedShow, setSelectedShow] = useState<SelectOptions>({
-    label: 'Day',
-    value: '-1d',
-  });
+  const [selectedShowTimeRange, setSelectedShowTimeRange] = useState<SelectOption>(INITIAL_SHOW_TIME_RANGE_VALUE);
 
   const handleDimensionModalOpen = () => {
     setModalName(ModalName.Dimensions);
@@ -255,13 +259,13 @@ export const MetricsExplorer: React.FC = () => {
 
   const handleTimeTabChange = (event, newValue: TimeMetricTabValue) => setTimeTab(newValue);
 
-  const handleLookbackChange = (value: SelectOptions) => setSelectedLookback(value);
+  const handleLookbackTimeRangeChange = (value: SelectOption) => setSelectedLookbackTimeRange(value);
 
-  const handleCustomFromDateChange = (value: string) => setSelectedCustomFromDate(value);
+  const handleCustomFromDateChange = (event: React.ChangeEvent<HTMLInputElement>) => setSelectedCustomFromDate(event.target.value);
 
-  const handleCustomToDateChange = (value: string) => setSelectedCustomToDate(value);
+  const handleCustomToDateChange = (event: React.ChangeEvent<HTMLInputElement>) => setSelectedCustomToDate(event.target.value);
 
-  const handleShowChange = (value: SelectOptions) => setSelectedShow(value);
+  const handleTimeRangeChange = (value: SelectOption) => setSelectedShowTimeRange(value);
 
   const customizationtabOptions: CustomizationTabProps[] = [
     {
@@ -337,30 +341,30 @@ export const MetricsExplorer: React.FC = () => {
           <Tabs classes={{ root: classes.timeTabContainer, indicator: classes.indicator }} value={timeTab} onChange={handleTimeTabChange} indicatorColor="primary">
             <Tab
               classes={{ selected: classes.selectedTab }}
-              value={TimeMetricTabValue.lookback}
+              value={TimeMetricTabValue.Lookback}
               label={<span className={classes.tableHeaderText}>LOOKBACK</span>}
               wrapped
-              {...a11yProps(TimeMetricTabValue.lookback)}
+              {...a11yProps(TimeMetricTabValue.Lookback)}
             />
             <Tab
               classes={{ selected: classes.selectedTab }}
-              value={TimeMetricTabValue.custom}
+              value={TimeMetricTabValue.Custom}
               label={<span className={classes.tableHeaderText}>CUSTOM</span>}
               wrapped
-              {...a11yProps(TimeMetricTabValue.custom)}
+              {...a11yProps(TimeMetricTabValue.Custom)}
             />
           </Tabs>
-          <TabPanel value={timeTab} index={TimeMetricTabValue.lookback}>
-            <LookbackTimeTab lookback={selectedLookback} handleLookbackChange={handleLookbackChange} />
+          <TabPanel value={timeTab} name={TimeMetricTabValue.Lookback}>
+            <LookbackTimeTab timeRange={selectedLookbackTimeRange} handleTimeRangeChange={handleLookbackTimeRangeChange} />
           </TabPanel>
-          <TabPanel value={timeTab} index={TimeMetricTabValue.custom}>
+          <TabPanel value={timeTab} name={TimeMetricTabValue.Custom}>
             <CustomTimeTab
-              customFromDate={selectedCustomFromDate}
-              customToDate={selectedCustomToDate}
-              handleCustomFromDateChange={handleCustomFromDateChange}
-              handleCustomToDateChange={handleCustomToDateChange}
-              show={selectedShow}
-              handleShowChange={handleShowChange}
+              fromDate={selectedCustomFromDate}
+              toDate={selectedCustomToDate}
+              onFromDateChange={handleCustomFromDateChange}
+              onToDateChange={handleCustomToDateChange}
+              timeRange={selectedShowTimeRange}
+              onTimeRangeChange={handleTimeRangeChange}
             />
           </TabPanel>
         </>
@@ -404,7 +408,7 @@ export const MetricsExplorer: React.FC = () => {
   return (
     <div className={classes.metricsExplorerContainer}>
       <div className={classes.leftBox}>
-        <MetricsChart dimensions={dimensions} tableData={DUMMY_METRICS_TABLE_DATA} lookback={selectedLookback.label} />
+        <MetricsChart dimensions={dimensions} tableData={DUMMY_METRICS_TABLE_DATA} lookback={selectedLookbackTimeRange.label} />
       </div>
       <div className={classes.rightBox}>
         <div className={classes.rightContainerTitle}>Metrics Customization</div>
