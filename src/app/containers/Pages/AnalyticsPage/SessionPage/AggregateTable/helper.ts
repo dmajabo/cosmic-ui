0d413dabed1d2a6615525c3fe4e6ad8fbf40350c
yurii.rabchevskiy <yrabchevskiy@gmail.com>
2021-12-20 13:43:25 +0200

    add missed vendor icon

--- src/app/containers/Pages/AnalyticsPage/SessionPage/AggregateTable/helper.ts
+++ src/app/containers/Pages/AnalyticsPage/SessionPage/AggregateTable/helper.ts
@@ -19,7 +19,11 @@ export const buildAggregatedData = (data: ISession[], buckets: IBuckets[]): IAgg
 const buildRow = (item: ISession, buckets: IBuckets[]): IAggregateRow => {
   const _buckects: IBuckets[] = buckets && buckets.length ? buckets.filter(it => it.key === item.sessionId) : [];
   const _groupData = getGroupedData(_buckects);
-  const _vendors: IState[] = _groupData && _groupData.vendors ? Object.keys(_groupData.vendors).map(key => getNestedTableHeader(_groupData.vendors[key])) : [];
+  let _vendors: IState[] = _groupData && _groupData.vendors ? Object.keys(_groupData.vendors).map(key => getNestedTableHeader(_groupData.vendors[key])) : [];
+  if (!_vendors || !_vendors.length) {
+    const _item = getNestedTableHeader(item.deviceVendor);
+    _vendors = [_item];
+  }
   const _row: IAggregateRow = { session: item, data: _groupData.groupData, vendors: _vendors };
   return _row;
 };
