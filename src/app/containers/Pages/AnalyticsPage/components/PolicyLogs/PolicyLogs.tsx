21ee493ed8f6796db855bf0fd6836a0b95da21a8
Ajay-TechP <ajay.vaidya@techprescient.com>
2021-12-20 11:06:40 +0530

    added checkbox list to select displayed columns

--- src/app/containers/Pages/AnalyticsPage/components/PolicyLogs/PolicyLogs.tsx
+++ src/app/containers/Pages/AnalyticsPage/components/PolicyLogs/PolicyLogs.tsx
@@ -1,5 +1,5 @@
 import { Button, Typography } from '@material-ui/core';
-import React, { useState } from 'react';
+import React, { useEffect, useState } from 'react';
 import Select from 'react-select';
 import { AnalyticsStyles } from '../../AnalyticsStyles';
 import SearchIcon from '../../icons/metrics explorer/search.svg';
@@ -9,6 +9,8 @@ import { LookbackLabel, LookbackSelectOption, LookbackValue } from '../Metrics E
 import { AnomalyPolicyLogsTableData, Column, ColumnAccessor } from 'lib/api/http/SharedTypes';
 import { AnomalySLATestTable } from '../Anomalies/AnomalySLATestTable';
 import ArrowRightIcon from '@mui/icons-material/ArrowRight';
+import { CheckboxData } from '../Metrics Explorer/Dimensions';
+import { Checkbox, FormControlLabel, FormGroup, Popover } from '@mui/material';
 
 const REGEX = /[-[\]{}()*+?.,\\^$|#\s]/g;
 
@@ -111,10 +113,38 @@ const dropdownStyle = {
   }),
 };
 
+const initialCheckboxData: CheckboxData = logsTableColumns.reduce((accu, nextValue) => {
+  accu[nextValue.accessor] = true;
+  return accu;
+}, {});
+
+const COLUMNS_POPOVER = 'columns-popover';
+
 export const PolicyLogs: React.FC = () => {
   const classes = AnalyticsStyles();
   const [searchText, setSearchText] = useState<string>('');
   const [timeRange, setTimeRange] = useState<LookbackSelectOption>(INITIAL_ANOMALY_TIME_RANGE_VALUE);
+  const [columnCheckboxData, setColumnCheckboxData] = useState<CheckboxData>(initialCheckboxData);
+  const [selectedColumns, setSelectedColumns] = useState<Column[]>([]);
+  const [columnAnchorEl, setColumnAnchorEl] = useState<HTMLButtonElement | null>(null);
+
+  const handleColmunsClick = (event: React.MouseEvent<HTMLButtonElement>) => setColumnAnchorEl(event.currentTarget);
+
+  const handleColumnsClose = () => setColumnAnchorEl(null);
+
+  const isColumnsPopoverOpen = Boolean(columnAnchorEl);
+  const columnsPopoverId = isColumnsPopoverOpen ? COLUMNS_POPOVER : undefined;
+
+  useEffect(() => {
+    const newSelectedColumns = logsTableColumns.filter(item => columnCheckboxData[item.accessor]);
+    setSelectedColumns(newSelectedColumns);
+  }, [columnCheckboxData]);
+
+  const handleCheckboxChange = (event: React.ChangeEvent<HTMLInputElement>) =>
+    setColumnCheckboxData({
+      ...columnCheckboxData,
+      [event.target.name]: event.target.checked,
+    });
 
   //adds \ before every special character of string to use in regular expression
   const escapeRegExp = (value: string) => value.replace(REGEX, '\\$&');
@@ -185,18 +215,41 @@ export const PolicyLogs: React.FC = () => {
             </Typography>
             <img src={FilterIcon} alt="columns" />
           </Button>
-          <Button className={classes.policyLogsOtherButton} variant="contained" disableElevation>
+          <Button aria-describedby={columnsPopoverId} className={classes.otherButton} variant="contained" onClick={handleColmunsClick} disableElevation>
             <Typography className={classes.otherButtonText} noWrap>
               COLUMNS
             </Typography>
             <img src={ColumnsIcon} alt="columns" />
           </Button>
+          <Popover
+            id={columnsPopoverId}
+            open={isColumnsPopoverOpen}
+            onClose={handleColumnsClose}
+            anchorEl={columnAnchorEl}
+            anchorOrigin={{
+              vertical: 'bottom',
+              horizontal: 'left',
+            }}
+          >
+            <FormGroup className={classes.popoverContainer}>
+              {logsTableColumns
+                .filter(column => column.accessor !== ColumnAccessor.hits)
+                .map(item => (
+                  <FormControlLabel
+                    key={item.accessor}
+                    className={classes.popoverItem}
+                    control={<Checkbox checked={columnCheckboxData[item.accessor]} onChange={handleCheckboxChange} name={item.accessor} />}
+                    label={<span className={classes.popoverText}>{item.Header}</span>}
+                  />
+                ))}
+            </FormGroup>
+          </Popover>
           <span className={classes.anomalyTimeRangeText}>Show:</span>
           <Select styles={dropdownStyle} className={classes.inlineSelect} label="Single select" value={timeRange} options={TIME_RANGE_OPTIONS} onChange={handleTimeRangeChange} />
         </div>
       </div>
       <div className={classes.policyLogsTableContainer}>
-        <AnomalySLATestTable columns={logsTableColumns} data={logsTableData} sortableHeaders={['EDGE', 'USER']} />
+        <AnomalySLATestTable columns={selectedColumns} data={logsTableData} sortableHeaders={['EDGE', 'USER']} />
       </div>
     </div>
   );
