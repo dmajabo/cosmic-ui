056b56e49b3d49376b42597e4f623d83a82e6ea6
Ajay-TechP <ajay.vaidya@techprescient.com>
2022-01-14 18:34:56 +0530

    moved Connecters to Workflow Page

--- src/app/containers/Pages/AutomationPage/index.tsx
+++ src/app/containers/Pages/AutomationPage/index.tsx
@@ -1,14 +1,19 @@
 import React from 'react';
 import { AutomationProvider, useAutomationActions } from 'lib/hooks/Automation/useAutomationDataContext';
 import MainPage from './Page/MainPage';
+import { AccountsProvider, useAccountsActions } from 'lib/hooks/Accounts/useAccountsDataContext';
 
 interface IProps {}
 
 const AutomationPage: React.FC<IProps> = (props: IProps) => {
   const automationActions = useAutomationActions();
+  const accountsActions = useAccountsActions();
+
   return (
     <AutomationProvider actions={automationActions}>
-      <MainPage />
+      <AccountsProvider actions={accountsActions}>
+        <MainPage />
+      </AccountsProvider>
     </AutomationProvider>
   );
 };
