6c3c8f0cd2c6b2c645cb772c381649f139dc5ab1
Ajay-TechP <ajay.vaidya@techprescient.com>
2022-01-24 10:30:16 +0530

    bugfix for label

--- src/app/containers/Pages/MetricsPage/components/Sites/DeviceHealth.tsx
+++ src/app/containers/Pages/MetricsPage/components/Sites/DeviceHealth.tsx
@@ -62,8 +62,8 @@ export const DeviceHealth: React.FC<DeviceHealthProps> = ({ devices, timeRange }
     const startDate = metricsData.map(item => item.metrics[0]);
     const endDate = metricsData.map(item => item.metrics[item.metrics.length - 1]);
     if (startDate && endDate) {
-      const formattedStartDate = startDate.map(item => DateTime.fromFormat(item.time, INPUT_TIME_FORMAT).toUTC().toMillis());
-      const formattedEndDate = endDate.map(item => DateTime.fromFormat(item.time, INPUT_TIME_FORMAT).toUTC().toMillis());
+      const formattedStartDate = startDate.map(item => (item ? DateTime.fromFormat(item.time, INPUT_TIME_FORMAT).toUTC().toMillis() : Infinity));
+      const formattedEndDate = endDate.map(item => (item ? DateTime.fromFormat(item.time, INPUT_TIME_FORMAT).toUTC().toMillis() : 0));
       return `${DateTime.fromMillis(Math.min(...formattedStartDate)).toFormat(CHART_TIME_FORMAT)} to ${DateTime.fromMillis(Math.max(...formattedEndDate)).toFormat(
         CHART_TIME_FORMAT,
       )} (1 day interval)`;
