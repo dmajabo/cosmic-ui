98c9b7a0121c917165e8c3e35ff3b3fb417d68be
Swarup Bam <swarup@techprescient.com>
2022-02-28 16:54:42 +0530

    Application logos alignment fixed

--- src/app/containers/Pages/TopologyPage/TopoMapV2/Containers/Nodes/NodeWrapper/ApplicationNode/icons/Microsoft.tsx
+++ src/app/containers/Pages/TopologyPage/TopoMapV2/Containers/Nodes/NodeWrapper/ApplicationNode/icons/Microsoft.tsx
@@ -1,12 +1,14 @@
 export const Microsoft = () => {
   return (
-    <g transform="translate(45,30)">
-      <svg width="80" height="80" xmlns="http://www.w3.org/2000/svg" aria-label="Microsoft" viewBox="0 0 512 512">
-        <path d="M75 75v171h171v-171z" fill="#f25022" />
-        <path d="M266 75v171h171v-171z" fill="#7fba00" />
-        <path d="M75 266v171h171v-171z" fill="#00a4ef" />
-        <path d="M266 266v171h171v-171z" fill="#ffb900" />
-      </svg>
-    </g>
+    <foreignObject style={{ height: '100px', width: '100px', transform: 'translate(71px, 30px)' }}>
+      <g>
+        <svg width="80" height="80" xmlns="http://www.w3.org/2000/svg" aria-label="Microsoft" viewBox="0 0 512 512">
+          <path d="M75 75v171h171v-171z" fill="#f25022" />
+          <path d="M266 75v171h171v-171z" fill="#7fba00" />
+          <path d="M75 266v171h171v-171z" fill="#00a4ef" />
+          <path d="M266 266v171h171v-171z" fill="#ffb900" />
+        </svg>
+      </g>
+    </foreignObject>
   );
 };
