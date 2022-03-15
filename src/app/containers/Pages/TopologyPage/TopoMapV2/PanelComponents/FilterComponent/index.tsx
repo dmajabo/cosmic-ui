27fa27ff788d728cdcbd4cf08fc828a7aed8fcc6
Swarup Bam <swarup@techprescient.com>
2022-03-15 10:53:35 +0530

    displaying filters based on the vendor type

--- src/app/containers/Pages/TopologyPage/TopoMapV2/PanelComponents/FilterComponent/index.tsx
+++ src/app/containers/Pages/TopologyPage/TopoMapV2/PanelComponents/FilterComponent/index.tsx
@@ -1,19 +1,22 @@
 import FilterGroup from 'app/components/Basic/FilterComponents/FilterGroup';
 import OverflowContainer from 'app/components/Basic/OverflowContainer/styles';
-// import { useTopologyV2DataContext } from 'lib/hooks/Topology/useTopologyDataContext';
 import { PanelContent } from 'app/components/Basic/PanelBar/styles';
-import { FilterEntityOptions, IMapped_Application, IMapped_Segment, TopoFilterTypes } from 'lib/hooks/Topology/models';
+import { accountFilterIcon, awsVendorIcon } from 'app/components/SVGIcons/topologyIcons/TopoMapV2Icons/filterPanelIcon';
+import { AccountVendorTypes } from 'lib/api/ApiModels/Accounts/apiModel';
+import { IMapped_Application, IMapped_Segment, TopoFilterTypes } from 'lib/hooks/Topology/models';
 import { useTopologyV2DataContext } from 'lib/hooks/Topology/useTopologyDataContext';
-import { cloneDeep } from 'lodash';
-import React from 'react';
+import { UserContext, UserContextState } from 'lib/Routes/UserProvider';
+import React, { useContext, useMemo } from 'react';
 import { PanelHeader, PanelTitle } from '../styles';
 import FilterApplicationGroup from './FilterApplicationGroup';
+import FilterNodesGroup from './FilterNodesGroup';
 import FilterSegmentsGroup from './FilterSegmentsGroup';
 
 interface IProps {}
 
 const FilterComponent: React.FC<IProps> = (props: IProps) => {
   const { topology } = useTopologyV2DataContext();
+  const userContext = useContext<UserContextState>(UserContext);
 
   const onSelectFilterOption = (groupType: TopoFilterTypes, v: any, selected: boolean) => {
     topology.onSelectFilterOption(groupType, v, selected);
@@ -27,12 +30,13 @@ const FilterComponent: React.FC<IProps> = (props: IProps) => {
     topology.onApplicationFilterOption(app, index, selected);
   };
 
-  //TODO: Do this if only Meraki is configured
-  const entities: FilterEntityOptions = cloneDeep(topology.entities);
-  entities.peer_connections.hide = true;
-  entities.transit.hide = true;
-  entities.vpc.hide = true;
-  entities.web_acls.hide = true;
+  const IS_VENDOR_AWS = useMemo(() => {
+    return userContext.vendors[AccountVendorTypes.AMAZON_AWS] ? true : false;
+  }, [userContext.vendors]);
+
+  const IS_VENDOR_MERAKI = useMemo(() => {
+    return userContext.vendors[AccountVendorTypes.CISCO_MERAKI] ? true : false;
+  }, [userContext.vendors]);
 
   return (
     <>
@@ -44,24 +48,30 @@ const FilterComponent: React.FC<IProps> = (props: IProps) => {
           {/* <FilterGroup maxGroupHeight="260px" defaultOpen label="Entities" styles={{ margin: '0 0 5px 0' }}>
             <FilterEntityGroup type={TopoFilterTypes.Entities} data={entities} onClick={onSelectFilterOption} />
           </FilterGroup> */}
-          <FilterGroup maxGroupHeight="unset" label="Site Segments" styles={{ margin: '0' }} defaultOpen={true}>
-            <FilterSegmentsGroup iconStyles={{ width: '20px', height: '20px' }} data={topology.segments} onClick={onSelectSegmentFilterOption} />
-          </FilterGroup>
-          <FilterGroup maxGroupHeight="unset" label="External Segments" styles={{ margin: '0' }} defaultOpen={true}>
-            <FilterApplicationGroup iconStyles={{ width: '20px', height: '20px' }} data={topology.applicationFilterOptions} onClick={onApplicationFilterOption} />
-          </FilterGroup>
+          {IS_VENDOR_MERAKI && (
+            <FilterGroup maxGroupHeight="unset" label="Site Segments" styles={{ margin: '0' }} defaultOpen={true}>
+              <FilterSegmentsGroup iconStyles={{ width: '20px', height: '20px' }} data={topology.segments} onClick={onSelectSegmentFilterOption} />
+            </FilterGroup>
+          )}
+          {IS_VENDOR_MERAKI && (
+            <FilterGroup maxGroupHeight="unset" label="External Segments" styles={{ margin: '0' }} defaultOpen={true}>
+              <FilterApplicationGroup iconStyles={{ width: '20px', height: '20px' }} data={topology.applicationFilterOptions} onClick={onApplicationFilterOption} />
+            </FilterGroup>
+          )}
           {/* <FilterGroup maxGroupHeight="260px" label="Health Severity" styles={{ margin: '0 0 5px 0' }}>
             <FilterSeverityGroup type={TopoFilterTypes.Severity} data={topology.severity} onClick={onSelectFilterOption} />
           </FilterGroup> */}
 
-          {/* TODO: Hide this when only Meraki is configured */}
-
-          {/* <FilterGroup maxGroupHeight="unset" label="Regions" styles={{ margin: '0 0 5px 0' }}>
-            <FilterNodesGroup type={TopoFilterTypes.Regions} icon={regionFilterIcon} data={topology.regions} onClick={onSelectFilterOption} />
-          </FilterGroup>
-          <FilterGroup maxGroupHeight="unset" label="Accounts" styles={{ margin: '0 0 5px 0' }}>
-            <FilterNodesGroup type={TopoFilterTypes.Accounts} icon={accountFilterIcon} data={topology.accounts} onClick={onSelectFilterOption} />
-          </FilterGroup> */}
+          {IS_VENDOR_AWS && (
+            <>
+              <FilterGroup maxGroupHeight="unset" label="Regions" styles={{ margin: '0 0 5px 0' }}>
+                <FilterNodesGroup type={TopoFilterTypes.Regions} icon={awsVendorIcon} data={topology.regions} onClick={onSelectFilterOption} />
+              </FilterGroup>
+              <FilterGroup maxGroupHeight="unset" label="Accounts" styles={{ margin: '0 0 5px 0' }}>
+                <FilterNodesGroup type={TopoFilterTypes.Accounts} icon={accountFilterIcon} data={topology.accounts} onClick={onSelectFilterOption} />
+              </FilterGroup>
+            </>
+          )}
         </PanelContent>
       </OverflowContainer>
     </>
